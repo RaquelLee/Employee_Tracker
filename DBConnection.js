dfc8d3ba147776d0b06b9f2608cbab7271622d99
rsco <RaquelLee@users.noreply.github.com>
2021-03-04 09:44:07 -0500

    create read and update employees

--- DBConnection.js
+++ DBConnection.js
@@ -8,16 +8,50 @@ const connection = mysql.createConnection({
     database: '',
 });
 
-const afterConnection = () => {
-    connection.query('SELECT FROM', (err, res) => {
-    if (err) throw err;
-    console.log(res);
-    connection.end();
+//view, create, update
+const readProducts = () => {
+    console.log('Selecting all employees...\n');
+    connection.query('SELECT * FROM employees', (err, res) => {
+        if (err) throw err;
+        console.log(res);
+        connection.end();
     });
+};
+
+const updateEmployeeRole = () => {
+    console.log('Updating...\n');
+    const query = connection.query(
+        'UPDATE products SET ? WHERE ?',
+        [
+        {
+        }
+        ],
+        (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} employees updated!\n`);
+            deleteProduct();
+        }
+    );
+    console.log(query.sql);
+};
+
+const createEmployee = () => {
+    console.log('Inserting new emplpoyee...\n');
+    const query = connection.query(
+        'INSERT INTO x SET ?',
+        {
+
+        },
+        (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} employee inserted!\n`);
+        }
+    );
+    console.log(query.sql);
 };
 
 connection.connect((err) => {
     if (err) throw err;
-    console.log(`connected as id ${connection.threadId}`);
-    afterConnection();
+    console.log(`connected as id ${connection.threadId}\n`);
+    createEmployee();
 });
